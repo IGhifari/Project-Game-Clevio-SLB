d399012268d29f11783dcdd5e082e0231ee478a6
IGhifari <rinnitoshii07@gmail.com>
2025-05-04 14:06:11 +0700

    menambahlan tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-hello word
+      <div className='text-amber-300'>
+        hello
+      </div>
     </>
   )
 }
